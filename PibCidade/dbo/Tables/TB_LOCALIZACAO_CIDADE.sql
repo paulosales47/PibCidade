89425ffdb829da37c351852b788fee18ca973017
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-02-13 14:09:06 -0200

    Adicionando tabela TB_LOCALIZACAO_CIDADE

---
+++ PibCidade/dbo/Tables/TB_LOCALIZACAO_CIDADE.sql
@@ -0,0 +1,16 @@
+﻿/*
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[TB_LOCALIZACAO_CIDADE]
+(
+	[ID_CIDADE] INT NOT NULL, 
+    [LATITUDE] FLOAT NOT NULL, 
+    [LONGITUDE] FLOAT NOT NULL, 
+    [COORDENADA_EXTENSO] VARCHAR(50) NULL, 
+    CONSTRAINT [FK_TB_LOCALIZACAO_CIDADE_TB_CIDADE] FOREIGN KEY ([ID_CIDADE]) REFERENCES [TB_CIDADE]([ID_CIDADE]) 
+) 
+
+GO
