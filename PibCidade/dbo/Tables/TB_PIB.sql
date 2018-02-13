d742f62e8e9e7fed7355906e23db08fc60fa865b
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-02-13 13:31:34 -0200

    Add project files.

---
+++ PibCidade/dbo/Tables/TB_PIB.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TB_PIB] (
+    [ID_PIB]              INT           IDENTITY (1, 1) NOT NULL,
+    [POSICAO]             SMALLINT      NOT NULL,
+    [CIDADE]              VARCHAR (MAX) NOT NULL,
+    [VALOR]               MONEY         NOT NULL,
+    [PERCENTUAL_RELATIVO] FLOAT (53)    NOT NULL,
+    [PERCENTUAL_TOTAL]    FLOAT (53)    NOT NULL,
+    [COORDENADA_X]        FLOAT (53)    NULL,
+    [COORDENADA_Y]        FLOAT (53)    NULL,
+    CONSTRAINT [PK_PIB] PRIMARY KEY CLUSTERED ([ID_PIB] ASC)
+);
+
